f5ae5675df66f431676e6a181187f098f7a39607
Aphollo138 <rafinhasimplicio03@gmail.com>
2025-05-09 10:11:55 -0300

    change server url

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -82,7 +82,7 @@ const handleLogin = (event) => {
   login.style.display = "none";
   chat.style.display = "flex";
 
-  websocket = new WebSocket("ws://localhost:8080");
+  websocket = new WebSocket("wss://chat-backend-3q0c.onrender.com");
   websocket.onmessage = processMessage;
 };
 
